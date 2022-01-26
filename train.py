e08c3215bd7b34a419171f7418160db54d935ce5
Bugra Sefercik <bsefercik@gmail.com>
2022-01-26 10:17:57 +0300

    add training script

--- train.py
+++ train.py
@@ -1,14 +1,20 @@
 import random
+import time
+import json
+import traceback
+import statistics
+import datetime
 from collections import defaultdict
 
 import torch
 import torch.optim as optim
+from torch.utils.data import DataLoader
 import numpy as np
 import MinkowskiEngine as ME
 import open3d as o3d
 from tensorboardX import SummaryWriter
 
-from utils import config, logger, utils
+from utils import config, logger, utils, metrics
 
 
 import ipdb
@@ -21,7 +27,10 @@ _tensorboard_writer = SummaryWriter(_config.exp_path)
 _use_cuda = torch.cuda.is_available()
 _device = torch.device("cuda" if _use_cuda else "cpu")
 
-def normalize_color(color: torch.Tensor, is_color_in_range_0_255: bool = False) -> torch.Tensor:
+
+def normalize_color(
+    color: torch.Tensor, is_color_in_range_0_255: bool = False
+) -> torch.Tensor:
     r"""
     Convert color in range [0, 1] to [-0.5, 0.5]. If the color is in range [0,
     255], use the argument `is_color_in_range_0_255=True`.
@@ -34,15 +43,158 @@ def normalize_color(color: torch.Tensor, is_color_in_range_0_255: bool = False) 
     color -= 0.5
     return color.float()
 
+
 def load_file(file_name):
     pcd = o3d.io.read_point_cloud(file_name)
     coords = np.array(pcd.points)
     colors = np.array(pcd.colors)
     return coords, colors, pcd
 
-if __name__ == '__main__':
-    _logger.info('Starting new training.')
-    _logger.info(f'CONFIG: {_config()}')
+
+def train_epoch(train_data_loader, model, optimizer, criterion, epoch):
+    iter_time = utils.AverageMeter()
+    data_time = utils.AverageMeter()
+    am_dict = defaultdict(utils.AverageMeter)
+
+    train_iter = iter(train_data_loader)
+    model.train()
+
+    start_epoch = time.time()
+    end = time.time()
+
+    for i, batch in enumerate(train_iter):
+
+        try:
+            data_time.update(time.time() - end)
+
+            utils.step_learning_rate(
+                optimizer,
+                _config.TRAIN.lr,
+                epoch - 1,
+                _config.TRAIN.step_epoch,
+                _config.TRAIN.multiplier,
+            )
+
+            coords, feats, _, poses, _ = batch
+            poses = poses.to(device=_device)
+
+            model_input = ME.SparseTensor(feats, coordinates=coords, device=_device)
+            out = model(model_input)
+
+            optimizer.zero_grad()
+            loss = criterion(out.F, poses)
+            loss.backward()
+            optimizer.step()
+
+            dists = metrics.compute_pose_dist(poses, out.features)
+            am_dict["loss"].update(loss.item(), len(poses))
+            am_dict["dist"].update(statistics.mean(dists[0].tolist()), len(poses))
+            am_dict["dist_position"].update(
+                statistics.mean(dists[1].tolist()), len(poses)
+            )
+            am_dict["dist_orientation"].update(
+                statistics.mean(dists[2].tolist()), len(poses)
+            )
+            am_dict["angle_diff"].update(statistics.mean(dists[3].tolist()), len(poses))
+
+            current_iter = (epoch - 1) * len(train_data_loader) + i + 1
+            max_iter = _config.TRAIN.epochs * len(train_data_loader)
+            remain_iter = max_iter - current_iter
+
+            iter_time.update(time.time() - end)
+            end = time.time()
+
+            remain_time = remain_iter * iter_time.avg
+            t_m, t_s = divmod(remain_time, 60)
+            t_h, t_m = divmod(t_m, 60)
+            remain_time = f"{int(t_h):02d}:{int(t_m):02d}:{int(t_s):02d}"
+
+            _logger.info(
+                "epoch: {}/{} iter: {}/{} loss: {:.4f}({:.4f}) data_time: {:.2f}({:.2f}) iter_time: {:.2f}({:.2f}) remain_time: {remain_time}".format(
+                    epoch,
+                    _config.TRAIN.epochs,
+                    i + 1,
+                    len(train_data_loader),
+                    am_dict["loss"].val,
+                    am_dict["loss"].avg,
+                    data_time.val,
+                    data_time.avg,
+                    iter_time.val,
+                    iter_time.avg,
+                    remain_time=remain_time,
+                )
+            )
+        # For better debugging
+        except Exception as e:
+            print(str(batch))
+            print(str(e))
+            print(traceback.format_exc())
+            ipdb.set_trace()
+            raise e
+        # except Exception:
+        #     _logger.exception(str(batch))
+
+    for k in am_dict:
+        # if k in visual_dict.keys():
+        _tensorboard_writer.add_scalar(k + "_train", am_dict[k].avg, epoch)
+    _tensorboard_writer.flush()
+
+
+def eval_epoch(val_data_loader, model, criterion, epoch):
+    _logger.info(f"> Evaluation at epoch: {epoch}")
+    am_dict = defaultdict(utils.AverageMeter)
+
+    with torch.no_grad():
+        val_iter = iter(val_data_loader)
+        model.eval()
+        start_epoch = time.time()
+        for i, batch in enumerate(val_iter):
+            try:
+                coords, feats, _, poses, _ = batch
+                poses = poses.to(device=_device)
+
+                model_input = ME.SparseTensor(
+                    feats, coordinates=coords, device=_device, requires_grad=False
+                )
+                out = model(model_input)
+                loss = criterion(out.F, poses)
+
+                dists = metrics.compute_pose_dist(poses, out.features)
+                am_dict["loss"].update(loss.item(), len(poses))
+                am_dict["dist"].update(statistics.mean(dists[0].tolist()), len(poses))
+                am_dict["dist_position"].update(
+                    statistics.mean(dists[1].tolist()), len(poses)
+                )
+                am_dict["dist_orientation"].update(
+                    statistics.mean(dists[2].tolist()), len(poses)
+                )
+                am_dict["angle_diff"].update(
+                    statistics.mean(dists[3].tolist()), len(poses)
+                )
+
+                _logger.info(
+                    f'iter: {i + 1}/{len(val_data_loader)} loss: {am_dict["loss"].val:.4f}({am_dict["loss"].avg:.4f})'
+                )
+            except Exception:
+                _logger.exception(str(batch))
+
+        _logger.info(
+            f'epoch: {epoch}/{_config.TRAIN.epochs}, val loss: {am_dict["loss"].avg:.4f}, time: {time.time() - start_epoch}s'
+        )
+
+        for k in am_dict:
+            # if k in visual_dict.keys():
+            _tensorboard_writer.add_scalar(k + "_val", am_dict[k].avg, epoch)
+        _tensorboard_writer.flush()
+
+
+if __name__ == "__main__":
+    _logger.info("=================================================\n")
+    _logger.info(f"UTC Time: {datetime.datetime.utcnow().isoformat()}")
+    _logger.info(f"Device: {_device}")
+    _logger.info("Starting new training.")
+
+    _logger.info(f"CONFIG: {_config()}")
 
     _logger.info(f"Setting seed: {_config.GENERAL.seed}")
     random.seed(_config.GENERAL.seed)
@@ -54,106 +206,71 @@ if __name__ == '__main__':
         torch.cuda.empty_cache()
 
     from model.robotnet import RobotNet, get_criterion
-    from data.alivev1 import AliveV1Dataset
+    from data.alivev1 import AliveV1Dataset, collate
 
-    voxel_size = 0.02
-    N_labels = 7
-
-    # criterion = nn.MSELoss()
     criterion = get_criterion(device=_device)
-    model = RobotNet(in_channels=3, out_channels=N_labels, D=3)
-    # print(model)
+    model = RobotNet(in_channels=3, out_channels=7, D=3)
+    _logger.info(f"Model: {str(model)}")
 
-    model = model.to(_device)
-
-    if _config.TRAIN.optim == 'Adam':
+    if _config.TRAIN.optim == "Adam":
         optimizer = optim.Adam(
-            filter(lambda p: p.requires_grad, model.parameters()),
-            lr=_config.TRAIN.lr
+            filter(lambda p: p.requires_grad, model.parameters()), lr=_config.TRAIN.lr
         )
-    elif _config.TRAIN.optim == 'SGD':
+    elif _config.TRAIN.optim == "SGD":
         optimizer = optim.SGD(
             filter(lambda p: p.requires_grad, model.parameters()),
             lr=_config.TRAIN.lr,
             momentum=_config.TRAIN.momentum,
-            weight_decay=_config.TRAIN.weight_decay
+            weight_decay=_config.TRAIN.weight_decay,
         )
 
-    train_dataset = AliveV1Dataset()
+    train_dataset = AliveV1Dataset(set_name="train")
+    train_data_loader = DataLoader(
+        train_dataset,
+        batch_size=_config.DATA.batch_size,
+        collate_fn=collate,
+        num_workers=_config.DATA.workers,
+        shuffle=True,
+        drop_last=True,
+        pin_memory=True,
+        worker_init_fn=utils.seed_worker,
+        generator=utils.torch_generator,
+    )
+    val_dataset = AliveV1Dataset(set_name="val")
+    val_data_loader = DataLoader(
+        val_dataset,
+        batch_size=_config.DATA.batch_size,
+        collate_fn=collate,
+        num_workers=max(2, int(_config.DATA.workers/4)),
+        shuffle=False,
+        drop_last=False,
+        pin_memory=True,
+    )
 
-    ipdb.set_trace()
+    start_epoch = utils.checkpoint_restore(
+        model,
+        _config.exp_path,
+        _config.config.split('/')[-1][:-5],
+        optimizer=optimizer,
+        use_cuda=_use_cuda
+    )  # resume from the latest epoch, or specify the epoch to restore
 
-    coords, colors, pcd = load_file("1.ply")
-    coords = torch.from_numpy(coords)
-    # # Get new data
-    # coordinates = ME.utils.batched_coordinates(
-    #     [coords[:10000]],
-    #     dtype=torch.float32,
-    #     device=_device
-    # )
+    for epoch in range(start_epoch, _config.TRAIN.epochs + 1):
+        train_epoch(train_data_loader, model, optimizer, criterion, epoch)
 
-    colors = torch.from_numpy(colors).to(_device, dtype=torch.float32)
-    colors = normalize_color(colors)
-    features = colors
-    # features = torch.cat((colors[:100], colors[:100]), dim=0)
+        if utils.is_multiple(epoch, _config.GENERAL.save_freq) or utils.is_power2(epoch):
+            utils.checkpoint_save(
+                model,
+                _config.exp_path,
+                _config.config.split('/')[-1][:-5],
+                epoch,
+                optimizer=optimizer,
+                save_freq=_config.GENERAL.save_freq,
+                use_cuda=_use_cuda
+            )
 
-    # dummy_label = torch.randint(0, N_labels, (2,), device=_device)
+            eval_epoch(val_data_loader, model, criterion, epoch)
 
-    # input = ME.SparseTensor(features, coordinates, device=_device)
-
-    # # Forward
-    # output = model(input)
     # ipdb.set_trace()
-    # in_field = ME.TensorField(
-    #     features=normalize_color(features),
-    #     coordinates=ME.utils.batched_coordinates([coords / voxel_size, coords], dtype=torch.float32),
-    #     quantization_mode=ME.SparseTensorQuantizationMode.UNWEIGHTED_AVERAGE,
-    #     minkowski_algorithm=ME.MinkowskiAlgorithm.SPEED_OPTIMIZED,
-    #     device=_device
-    # )
-    # # Convert to a sparse tensor
-    # sinput = in_field.sparse()
-    # # Output sparse tensor
-    # soutput = model(sinput)
-    # # get the prediction on the input tensor field
-    # out_field = soutput.slice(in_field)
-    # # logits = out_field.F
-    # ipdb.set_trace()
-    # Loss
-    # loss = criterion(output.F, dummy_label)
 
-    # ipdb.set_trace()
-    for i in range(500):
-        optimizer.zero_grad()
-
-
-        in_field = ME.TensorField(
-            features=features,
-            coordinates=ME.utils.batched_coordinates([coords / voxel_size], dtype=torch.float32),
-            quantization_mode=ME.SparseTensorQuantizationMode.UNWEIGHTED_AVERAGE,
-            minkowski_algorithm=ME.MinkowskiAlgorithm.SPEED_OPTIMIZED,
-            # minkowski_algorithm=ME.MinkowskiAlgorithm.MEMORY_EFFICIENT,
-            device=_device,
-            requires_grad=True
-        )
-        # Convert to a sparse tensor
-        sinput = in_field.sparse()
-        # Output sparse tensor
-        soutput = model(sinput)
-        if i == 130:
-            ipdb.set_trace()
-        # out_field = soutput.slice(in_field)
-        dummy_label = torch.randint(0, 1, soutput.F.size(), device=_device, dtype=torch.float32)
-        # ipdb.set_trace()
-        # Loss
-        loss = criterion(soutput.F, dummy_label)
-
-        print("Iteration: ", i, ", Loss: ", loss.item())
-
-        # Gradient
-        loss.backward()
-        optimizer.step()
-
-
-
-    print('no error')
+    _logger.info("DONE!")
