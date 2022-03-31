16a4af6efc0a6a99bbfa22eef8c1802c029d88d5
Bugra Sefercik <bugra.sefercik@udemy.com>
2022-03-31 12:37:14 +0300

    add pickle visualization

---
+++ visualization/viz_pickle.py
@@ -0,0 +1,59 @@
+import sys
+import os
+import copy
+
+import numpy as np
+import open3d as o3d
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from utils import file_utils
+
+
+if __name__ == "__main__":
+    (
+        points,
+        rgb,
+        labels,
+        instance_label,
+        pose,
+    ), semantic_pred = file_utils.load_alive_file(sys.argv[1])
+
+    # arm_idx = labels == 1
+    arm_idx = labels > -1
+    points = points[arm_idx]
+    rgb = rgb[arm_idx]
+    labels = [arm_idx]
+
+    pcd = o3d.geometry.PointCloud()
+
+    frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.25)
+    ee_position = pose[:3]
+    ee_orientation = pose[3:].tolist()
+    ee_orientation = ee_orientation[-1:] + ee_orientation[:-1]
+    ee_frame = copy.deepcopy(frame).translate(ee_position)
+    ee_frame.rotate(frame.get_rotation_matrix_from_quaternion(ee_orientation))
+    kinect_frame = copy.deepcopy(frame).translate([0, 0, 0])
+    kinect_frame.rotate(frame.get_rotation_matrix_from_quaternion([0] * 4))
+
+    arm_mask = points
+    arm_mask = points[:, 0] > -500
+    arm_mask = np.logical_and(points[:, 0] < 0.5, arm_mask)  # x
+    arm_mask = np.logical_and(points[:, 0] > -0.5, arm_mask)
+    arm_mask = np.logical_and(points[:, 1] < 0.27, arm_mask)  # y
+    # # arm_mask = np.logical_and(points[:, 1] > 0.2, arm_mask)
+    arm_mask = np.logical_and(points[:, 2] < 1.3, arm_mask)  # z
+    # # # arm_mask = np.logical_and(points[:, 2] > 1.5, arm_mask)
+
+    points = points[arm_mask]
+    rgb = rgb[arm_mask]
+
+    def switch_to_normal(vis):
+        pcd.colors = o3d.utility.Vector3dVector(rgb)
+        vis.update_geometry(pcd)
+        return False
+
+    pcd.points = o3d.utility.Vector3dVector(points)
+    pcd.colors = o3d.utility.Vector3dVector(rgb)
+
+    key_to_callback = {ord("K"): switch_to_normal}
+    o3d.visualization.draw_geometries_with_key_callbacks([pcd, ee_frame, kinect_frame], key_to_callback)
