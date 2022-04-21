a694d21cc67768a6710dd9940f9e8d88a271a452
Bugra Sefercik <bugra.sefercik@udemy.com>
2022-04-21 13:43:15 +0300

    add save at keyboard interrupt for pickle picker

--- scripts/pickle_picker.py
+++ scripts/pickle_picker.py
@@ -47,54 +47,56 @@ if __name__ == "__main__":
     frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.25)
     for s in splits:
         for i, ins in enumerate(splits[s]):
-            print(ins['filepath'])
+            try:
+                print(ins['filepath'])
 
-            if all(ins.get(k, False) for k in new_fields):
-                print('Skipped')
-                continue
+                if all(ins.get(k, False) for k in new_fields):
+                    print('Skipped')
+                    continue
 
-            filepath = fix_filepath(ins['filepath'])
+                filepath = fix_filepath(ins['filepath'])
 
-            (
-                points,
-                rgb,
-                labels,
-                instance_label,
-                pose,
-            ), semantic_pred = file_utils.load_alive_file(filepath)
+                (
+                    points,
+                    rgb,
+                    labels,
+                    instance_label,
+                    pose,
+                ), semantic_pred = file_utils.load_alive_file(filepath)
 
-            if rgb.min() < 0:
-                # WRONG approach, tries to shit from data prep code.
-                rgb[:, 0] = preprocessing.minmax_scale(rgb[:, 0], feature_range=(0, 1), axis=0)
-                rgb[:, 1] = preprocessing.minmax_scale(rgb[:, 1], feature_range=(0, 1), axis=0)
-                rgb[:, 2] = preprocessing.minmax_scale(rgb[:, 2], feature_range=(0, 1), axis=0)
+                if rgb.min() < 0:
+                    # WRONG approach, tries to shit from data prep code.
+                    rgb[:, 0] = preprocessing.minmax_scale(rgb[:, 0], feature_range=(0, 1), axis=0)
+                    rgb[:, 1] = preprocessing.minmax_scale(rgb[:, 1], feature_range=(0, 1), axis=0)
+                    rgb[:, 2] = preprocessing.minmax_scale(rgb[:, 2], feature_range=(0, 1), axis=0)
 
-            ee_frame = get_frame_from_pose(frame, pose, switch_w=True)
-            kinect_frame = get_frame_from_pose(frame, [0] * 7)
+                ee_frame = get_frame_from_pose(frame, pose, switch_w=True)
+                kinect_frame = get_frame_from_pose(frame, [0] * 7)
 
-            pcd = o3d.geometry.PointCloud()
+                pcd = o3d.geometry.PointCloud()
 
-            roi_mask = get_roi_mask(points, **roi)
-            points = points[roi_mask]
-            rgb = rgb[roi_mask]
+                roi_mask = get_roi_mask(points, **roi)
+                points = points[roi_mask]
+                rgb = rgb[roi_mask]
 
+                pcd.points = o3d.utility.Vector3dVector(points)
+                pcd.colors = o3d.utility.Vector3dVector(rgb)
 
-            pcd.points = o3d.utility.Vector3dVector(points)
-            pcd.colors = o3d.utility.Vector3dVector(rgb)
+                o3d.visualization.draw_geometries_with_key_callbacks(
+                    [pcd, ee_frame, kinect_frame], key_to_callback
+                )
 
-            o3d.visualization.draw_geometries_with_key_callbacks(
-                [pcd, ee_frame, kinect_frame], key_to_callback
-            )
+                position_ok = input("Is position OK? [Y/n]: ")
+                orientation_ok = input("Is orientation OK? [Y/n]: ")
 
-            # print("Is position OK? [Y/n]:")
-            position_ok = input("Is position OK? [Y/n]: ")
-            orientation_ok = input("Is orientation OK? [Y/n]: ")
+                splits[s][i]['position_eligibility'] = position_ok.lower() in ('', 'yes', 'y')
+                splits[s][i]['orientation_eligibility'] = orientation_ok.lower() in ('', 'yes', 'y')
 
-            splits[s][i]['position_eligibility'] = position_ok.lower() in ('', 'yes', 'y')
-            splits[s][i]['orientation_eligibility'] = orientation_ok.lower() in ('', 'yes', 'y')
-
-            if i % args.save_freq == 0:
+                if i % args.save_freq == 0:
+                    save_file(args.splits, splits)
+            except KeyboardInterrupt:
                 save_file(args.splits, splits)
+                raise KeyboardInterrupt
 
         save_file(args.splits, splits)
 
