e1576a0925769eded471849f42c6d62c00fe94e5
Bugra Sefercik <bsefercik@gmail.com>
2022-04-24 20:59:01 +0300

    add vector normalization in metric calc

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn.functional as F
 import ipdb
 
 EPS = 1e-7
@@ -11,16 +12,15 @@ def compute_pose_dist(gt, pred):
         position_pred = pred[:, :3]
         orientation_pred = pred[:, 3:7]
 
+        gt_orientation_normalized = F.normalize(orientation, p=2, dim=1)
+        yorientation__pred_normalized = F.normalize(orientation_pred, p=2, dim=1)
+
         dist = torch.norm(gt - pred[:, :7], dim=1)
         dist_position = torch.norm(position - position_pred, dim=1)
         dist_orientation = torch.norm(orientation - orientation_pred, dim=1)
 
         angle_diff = torch.acos(
-            torch.clamp(
-                2 * (torch.sum(orientation * orientation_pred, dim=1) ** 2) - 1,
-                min=-1 + EPS,
-                max=1 - EPS,
-            )
+            2 * (torch.sum(gt_orientation_normalized * yorientation__pred_normalized, dim=1) ** 2) - 1,
         )
 
         return dist, dist_position, dist_orientation, angle_diff
