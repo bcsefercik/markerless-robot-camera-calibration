f415d43c34f1f0af3a198e68a48dcc6f0ae785ed
Bugra Sefercik <bugra.sefercik@udemy.com>
2022-04-26 13:44:42 +0300

    enhance viz pcd

--- visualization/viz_pcd.py
+++ visualization/viz_pcd.py
@@ -6,8 +6,18 @@ import copy
 import numpy as np
 import open3d as o3d
 
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from utils.data import get_roi_mask
+from visualization.viz_pickle import get_frame_from_pose
 
 if __name__ == "__main__":
+
+    limits = {
+        "min_x": -0.5,
+        "max_x": 0.3,
+        "max_z": 1.3,
+        "min_y": -0.5
+    }
 
     frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.25)
     # frame = frame.rotate(frame.get_rotation_matrix_from_xyz((np.pi,0,np.pi/4)))
@@ -15,60 +25,16 @@ if __name__ == "__main__":
         ee = np.load(sys.argv[1].replace(".pcd", ".npy"), allow_pickle=True)
     else:
         ee = np.zeros((7))
-    ee_position = ee[:3]
-    ee_orientation = ee[3:].tolist()
-    ee_orientation = ee_orientation[-1:] + ee_orientation[:-1]
-    ee_frame = copy.deepcopy(frame).translate(ee_position)
-    ee_frame.rotate(frame.get_rotation_matrix_from_quaternion(ee_orientation))
+
+    ee_frame = get_frame_from_pose(frame, ee, switch_w=True)
+    kinect_frame = get_frame_from_pose(frame, [0] * 7)
 
     pcd = o3d.io.read_point_cloud(sys.argv[1])
-    # bg = o3d.io.read_point_cloud(sys.argv[2])
-    # ipdb.set_trace()
 
     points = np.asarray(pcd.points)
     rgb = np.asarray(pcd.colors)
 
-    roi_mask = points[:, 0] > -500
-
-    # c1_p1
-    roi_mask = np.logical_and(points[:, 0] < 0.43, roi_mask)  # x
-    roi_mask = np.logical_and(points[:, 0] > -0.6, roi_mask)
-    roi_mask = np.logical_and(points[:, 1] < 0.3, roi_mask)  # y
-    # # # roi_mask = np.logical_and(points[:, 1] > -0.02, roi_mask)
-    roi_mask = np.logical_and(points[:, 2] < 1.2, roi_mask)  # z
-    roi_mask = np.logical_and(points[:, 2] > 0, roi_mask)
-
-    # p2_1
-    # roi_mask = np.logical_and(points[:, 0] < 0.39, roi_mask)  # x
-    # roi_mask = np.logical_and(points[:, 0] > -0.52, roi_mask)
-    # roi_mask = np.logical_and(points[:, 1] < 0.27, roi_mask)  # y
-    # # # # roi_mask = np.logical_and(points[:, 1] > -0.02, roi_mask)
-    # roi_mask = np.logical_and(points[:, 2] < 1.15, roi_mask)  # z
-    # roi_mask = np.logical_and(points[:, 2] > 0.3, roi_mask)
-
-    # p3_1
-    # roi_mask = np.logical_and(points[:, 0] < 0.22, roi_mask)  # x
-    # roi_mask = np.logical_and(points[:, 0] > -0.52, roi_mask)
-    # roi_mask = np.logical_and(points[:, 1] < 0.26, roi_mask)  # y
-    # # # roi_mask = np.logical_and(points[:, 1] > -0.02, roi_mask)
-    # roi_mask = np.logical_and(points[:, 2] < 1.2, roi_mask)  # z
-    # roi_mask = np.logical_and(points[:, 2] > 0, roi_mask)
-
-    # p2h2l1
-    # roi_mask = np.logical_and(points[:, 0] < 0.5, roi_mask)  # x
-    # roi_mask = np.logical_and(points[:, 0] > -0.5, roi_mask)
-    # roi_mask = np.logical_and(points[:, 1] < 0.27, roi_mask)  # y
-    # # # roi_mask = np.logical_and(points[:, 1] > 0.2, roi_mask)
-    # roi_mask = np.logical_and(points[:, 2] < 1.3, roi_mask)  # z
-    # # # # roi_mask = np.logical_and(points[:, 2] > 1.5, roi_mask)
-
-    # p2h3l1
-    # roi_mask = np.logical_and(points[:, 0] < 0.5, roi_mask)  # x
-    # roi_mask = np.logical_and(points[:, 0] > -0.5, roi_mask)
-    # roi_mask = np.logical_and(points[:, 1] < 0.27, roi_mask)  # y
-    # # roi_mask = np.logical_and(points[:, 1] > -0.02, roi_mask)
-    # roi_mask = np.logical_and(points[:, 2] < 1.27, roi_mask)  # z
-    # # roi_mask = np.logical_and(points[:, 2] > 1, roi_mask)
+    roi_mask = get_roi_mask(points, **limits)
 
     points = points[roi_mask]
     rgb = rgb[roi_mask]
@@ -79,7 +45,7 @@ if __name__ == "__main__":
     pcd_th.points = o3d.utility.Vector3dVector(points)
     pcd_th.colors = o3d.utility.Vector3dVector(rgb)
 
-    o3d.visualization.draw_geometries([pcd_th, ee_frame])
+    o3d.visualization.draw_geometries([pcd_th, ee_frame, kinect_frame])
 
     # ipdb.set_trace()
 
