5ad54089ae3a7526a51d10fad4081e4aeba75227
Bugra Sefercik <bugra.sefercik@udemy.com>
2022-05-19 21:12:14 +0300

    add elastic distortion augmentation

---
+++ utils/augmentation.py
@@ -0,0 +1,25 @@
+import numpy as np
+import scipy
+
+
+# Elastic distortion
+def distort_elastic(x, gran, mag):
+    blur0 = np.ones((3, 1, 1)).astype('float32') / 3
+    blur1 = np.ones((1, 3, 1)).astype('float32') / 3
+    blur2 = np.ones((1, 1, 3)).astype('float32') / 3
+
+    bb = np.abs(x).max(0).astype(np.int32)//gran + 3
+    noise = [np.random.randn(bb[0], bb[1], bb[2]).astype('float32') for _ in range(3)]
+    noise = [scipy.ndimage.filters.convolve(n, blur0, mode='constant', cval=0) for n in noise]
+    noise = [scipy.ndimage.filters.convolve(n, blur1, mode='constant', cval=0) for n in noise]
+    noise = [scipy.ndimage.filters.convolve(n, blur2, mode='constant', cval=0) for n in noise]
+    noise = [scipy.ndimage.filters.convolve(n, blur0, mode='constant', cval=0) for n in noise]
+    noise = [scipy.ndimage.filters.convolve(n, blur1, mode='constant', cval=0) for n in noise]
+    noise = [scipy.ndimage.filters.convolve(n, blur2, mode='constant', cval=0) for n in noise]
+    ax = [np.linspace(-(b-1)*gran, (b-1)*gran, b) for b in bb]
+    interp = [scipy.interpolate.RegularGridInterpolator(ax, n, bounds_error=0, fill_value=0) for n in noise]
+
+    def g(x_):
+        return np.hstack([i(x_)[:, None] for i in interp])
+
+    return x + g(x) * mag
