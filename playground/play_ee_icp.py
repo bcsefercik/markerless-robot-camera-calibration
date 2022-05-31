621651f2e503de2ed2667de315d2cb220222a005
Bugra Sefercik <bugra.sefercik@udemy.com>
2022-05-31 19:53:30 +0300

    add icp v1 play

--- playground/play_ee_icp.py
+++ playground/play_ee_icp.py
@@ -15,6 +15,14 @@ from utils.transformation import get_quaternion_rotation_matrix, get_transformat
 from utils.preprocess import center_at_origin
 
 
+def apply_noise(pcd, mu, sigma):
+    noisy_pcd = copy.deepcopy(pcd)
+    points = np.asarray(noisy_pcd.points)
+    points += np.random.normal(mu, sigma, size=points.shape)
+    noisy_pcd.points = o3d.utility.Vector3dVector(points)
+    return noisy_pcd
+
+
 if __name__ == "__main__":
     data, semantic_pred = file_utils.load_alive_file(sys.argv[1])
 
@@ -51,6 +59,12 @@ if __name__ == "__main__":
     ee_points = points[ee_idx]
     ee_rgb = rgb[ee_idx] * 0
 
+    pcd_ee = o3d.geometry.PointCloud()
+    pcd_ee.points = o3d.utility.Vector3dVector(ee_points)
+    pcd_ee.colors = o3d.utility.Vector3dVector(ee_rgb)
+    # must do this for point to plane icp
+    # pcd_ee.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.01, max_nn=5))
+
     new_ee_points = (rot_mat.T @ np.concatenate((ee_points, pose[:3].reshape(1, 3))).reshape((-1, 3, 1))).reshape((-1, 3))
     new_ee_points, origin_offset = center_at_origin(new_ee_points)
     new_ee_pose_pos = new_ee_points[-1]
@@ -66,19 +80,43 @@ if __name__ == "__main__":
     pcd.colors = o3d.utility.Vector3dVector(rgb_show)
 
     textured_mesh = o3d.io.read_triangle_mesh("../others/hand_files/hand.obj")
-    pcd_cad = textured_mesh.sample_points_uniformly(number_of_points=8192)  # it has normal since converted from mesh
-    pcd_cad = textured_mesh.sample_points_poisson_disk(number_of_points=4096, pcl=pcd_cad)
+    # textured_mesh = o3d.io.read_triangle_mesh("../others/hand_files/hand_notblender.obj")
 
-    pose_jiggled = pose + (np.random.rand(7) * 2 - 1) * 0.04
+    pcd_cad = textured_mesh.sample_points_uniformly(number_of_points=8192*2)  # has normal since converted from mesh
+    pcd_cad = textured_mesh.sample_points_poisson_disk(number_of_points=4096*2, pcl=pcd_cad)
+
+    pcd_cad_points = np.asarray(pcd_cad.points)
+    pcd_cad_normals = np.asarray(pcd_cad.normals)
+    pcd_cad_mask = (pcd_cad_points[:, 0] > 0.0) * (pcd_cad_points[:, 2] > -0.0)
+    pcd_cad.points = o3d.utility.Vector3dVector(pcd_cad_points[pcd_cad_mask])
+    pcd_cad.normals = o3d.utility.Vector3dVector(pcd_cad_normals[pcd_cad_mask])
+    # ipdb.set_trace()
+
+
+    pose_jiggled = pose + (np.random.rand(7) * 2 - 1) * 0.01
     trans_mat_jiggled = get_transformation_matrix(pose_jiggled, switch_w=True)  # switch_w=False in dataloader
 
     # pcd_cad.transform(trans_mat_jiggled)
 
-    # must do this for point to plane icp
-    pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=30))
 
+    mu, sigma = 0, 0.1
+    source_noisy = apply_noise(pcd_cad, mu, sigma)
+
+    threshold = 0.2
+    loss = o3d.pipelines.registration.TukeyLoss(k=sigma)
+
+    p2l = o3d.pipelines.registration.TransformationEstimationPointToPlane(loss)
+    reg_p2l = o3d.pipelines.registration.registration_icp(
+        pcd_cad, pcd_ee, threshold, trans_mat_jiggled,
+        o3d.pipelines.registration.TransformationEstimationPointToPoint())
+
+    pcd_cad.transform(reg_p2l.transformation)
+    # pcd_cad.transform(trans_mat_jiggled)
+    # pcd_cad.transform(gt_trans_mat)
     # ipdb.set_trace()
 
     o3d.visualization.draw_geometries(
-        [pcd, pcd_cad, kinect_frame, ee_frame]
+        [pcd_ee, pcd_cad, kinect_frame, ee_frame]
+        # [pcd, pcd_ee, pcd_cad, kinect_frame, ee_frame]
     )
+
