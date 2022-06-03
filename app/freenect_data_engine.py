2590761192d053bfcc70a8fb683b43277a8c61be
Bugra Sefercik <bsefercik@gmail.com>
2022-06-03 17:46:56 +0300

    add pose data collection to freenect engine

--- app/freenect_data_engine.py
+++ app/freenect_data_engine.py
@@ -13,6 +13,7 @@ from queue import Queue, Full, Empty
 import numpy as np
 import rospy
 from sensor_msgs.msg import PointCloud2
+from geometry_msgs.msg import PoseStamped
 import sensor_msgs.point_cloud2 as pc2
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -29,6 +30,7 @@ class FreenectDataEngine(DataEngineInterface):
         self.exit_event = threading.Event()
 
         self.pc2_queue = Queue(1)
+        self.ee_pose_queue = Queue(1)
         self.ready_queue = Queue(1)
 
         self._dto_thread = threading.Thread(target=self._dto_generation_thread)
@@ -43,6 +45,7 @@ class FreenectDataEngine(DataEngineInterface):
     def run(self):
         rospy.init_node("depth_registered_recorder")
         rospy.Subscriber("/camera/depth_registered/points", PointCloud2, self._callback)
+        rospy.Subscriber("/robot/ee_pose", PoseStamped, self._ee_pose_callback)
 
     def _dto_generation_thread(self):
         while not self.exit_event.is_set():
@@ -51,12 +54,28 @@ class FreenectDataEngine(DataEngineInterface):
             if data is None:
                 return
 
+            ee2base_data = None
+            try:
+                raw = self.ee_pose_queue.get_nowait()
+                ee2base_data = np.array([
+                    raw.pose.position.x,
+                    raw.pose.position.y,
+                    raw.pose.position.z,
+                    raw.pose.orientation.w,
+                    raw.pose.orientation.x,
+                    raw.pose.orientation.y,
+                    raw.pose.orientation.z
+                ])
+            except Empty:
+                pass
+
             points, colors = get_points_and_colors(data)
 
             new_dto = PointCloudDTO(
                 points=points,
                 rgb=colors / 255,
                 timestamp=datetime.utcnow(),
+                ee2base_pose=ee2base_data
             )
 
             try:
@@ -79,9 +98,17 @@ class FreenectDataEngine(DataEngineInterface):
         except Full:
             pass
 
+    def _ee_pose_callback(self, data):
+        try:
+            if not self.exit_event.is_set():
+                self.ee_pose_queue.put_nowait(data)
+        except Full:
+            pass
+
     def exit(self):
         self.exit_event.set()
         time.sleep(self.min_wait_time)
         self.pc2_queue.put(None)
-        time.sleep(0.1)
+        self.ee_pose_queue.put(None)
+        time.sleep(0.2)
         self._dto_thread.join()
