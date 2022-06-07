73852b82d2f01f36ddda129d74f6a2efdb292a2d
Bugra Sefercik <bugra.sefercik@udemy.com>
2022-06-07 18:16:31 +0300

    add hand pcd

--- utils/icp.py
+++ utils/icp.py
@@ -14,27 +14,30 @@ def get_point2point_matcher(cad_name="hand_notblender.obj"):
     # _cad_mesh = o3d.io.read_triangle_mesh(
     #     os.path.join(BASE_PATH, "..", "app", "hand_files", "hand_notblender.obj")
     # )
-    _cad_mesh = o3d.io.read_triangle_mesh(
-        os.path.join(
-            BASE_PATH, "..", "app", "hand_files", "hand_notblender.obj"
-        )  # seems to work better
-    )
+    if cad_name.endswith(".pcd"):
+        _cad_pcd = o3d.io.read_point_cloud(cad_name)
+    else:
+        _cad_mesh = o3d.io.read_triangle_mesh(
+            os.path.join(
+                BASE_PATH, "..", "app", "hand_files", "hand_notblender.obj"
+            )  # seems to work better
+        )
 
-    _cad_pcd = _cad_mesh.sample_points_uniformly(
-        number_of_points=16384
-    )  # has normal since converted from mesh
-    _cad_pcd = _cad_mesh.sample_points_poisson_disk(
-        number_of_points=8192, pcl=_cad_pcd
-    )
-    _pcd_cad_points = np.asarray(_cad_pcd.points)
-    _pcd_cad_normals = np.asarray(_cad_pcd.normals)
-    _pcd_cad_mask = _pcd_cad_points[:, 0] > 0.0 * (_pcd_cad_points[:, 2] > -0.01)
-    _cad_pcd.points = o3d.utility.Vector3dVector(
-        _pcd_cad_points[_pcd_cad_mask]
-    )
-    _cad_pcd.normals = o3d.utility.Vector3dVector(
-        _pcd_cad_normals[_pcd_cad_mask]
-    )
+        _cad_pcd = _cad_mesh.sample_points_uniformly(
+            number_of_points=16384
+        )  # has normal since converted from mesh
+        _cad_pcd = _cad_mesh.sample_points_poisson_disk(
+            number_of_points=8192, pcl=_cad_pcd
+        )
+        _pcd_cad_points = np.asarray(_cad_pcd.points)
+        _pcd_cad_normals = np.asarray(_cad_pcd.normals)
+        _pcd_cad_mask = _pcd_cad_points[:, 0] > 0.0 * (_pcd_cad_points[:, 2] > -0.01)
+        _cad_pcd.points = o3d.utility.Vector3dVector(
+            _pcd_cad_points[_pcd_cad_mask]
+        )
+        _cad_pcd.normals = o3d.utility.Vector3dVector(
+            _pcd_cad_normals[_pcd_cad_mask]
+        )
     _ee_pcd = o3d.geometry.PointCloud()
     _icp_threshold = 0.1
     _icp_method = (
