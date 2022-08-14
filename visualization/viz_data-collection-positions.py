dd0c116e1f337bca2259af6bbba4879d84515ec6
Bugra Sefercik <bugra.sefercik@udemy.com>
2022-08-14 21:33:29 +0300

    add arm cad model

--- visualization/viz_data-collection-positions.py
+++ visualization/viz_data-collection-positions.py
@@ -11,7 +11,7 @@ import numpy as np
 sys.path.append("..")  # noqa
 from utils import file_utils
 from utils.visualization import create_coordinate_frame, get_kinect_mesh
-from utils.transformation import get_pose_inverse, switch_w, transform_pose2pose
+from utils.transformation import get_pose_inverse, get_transformation_matrix, switch_w, transform_pose2pose
 from utils.data import get_roi_mask
 
 
@@ -43,24 +43,60 @@ if __name__ == "__main__":
 
     kinect_mesh = get_kinect_mesh(np.array([0, 0, 0, 1.0, 0, 0, 0]), coordinate_frame_enabled=True)
 
+    robot_mesh = o3d.io.read_triangle_mesh(
+        os.path.join(
+            BASE_PATH, "..", "app", "hand_files", "franka_emika_panda.obj"
+        )  # seems to work better
+    )
+    robot_mesh.paint_uniform_color(np.array([210, 218, 255])/255)
+    # robot_mesh.scale(0.001, np.array([0, 0, 0]))
+
+    reflect = np.matrix([
+        [1, 0, 0, 0],
+        [0, -1, 0, 0],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1]
+    ], dtype=np.float64)
+    k_rot_z = np.matrix([
+        [0, 1, 0],
+        [-1, 0, 0],
+        [0, 0, 1]
+    ], dtype=np.float64)
+
+    robot_mesh.rotate(k_rot_z)
+    # ipdb.set_trace()
+    robot_mesh.transform(reflect)
+    # robot_mesh.translate(np.array([0.3,  -0.24,  0.17]))
+    robot_mesh.translate(np.array([0., 0.,  0.0]))
+
+
+
     data, _ = file_utils.load_alive_file(args.ref)
     points = data['points']
     rgb = data['rgb']
     pose = data['pose']
+    labels = data['labels']
+    # points = points[labels == 0]
+    # rgb = rgb[labels == 0]
     pose = switch_w(pose)
     robot2ee_pose = data['robot2ee_pose']
     ee2robot_pose = get_pose_inverse(switch_w(robot2ee_pose))
     cam2base = transform_pose2pose(pose, ee2robot_pose)
+    cam2base_mtx = get_transformation_matrix(cam2base)
 
     kinects = [create_kinect(kp, cam2base, scale=0.0012) for kp in args.kinects + [args.ref]]
     # kinects = [
     #     get_kinect_mesh(cam2base, coordinate_frame_enabled=True)
     #     ]
 
+    robot_mesh.transform(cam2base_mtx)
+    # robot_mesh.rotate(k_rot_y)
+
     roi_mask = get_roi_mask(
         points,
         max_z=1.5,
-        max_y=0.5
+        max_y=0.5,
+        # min_x=-0.4,
     )
 
     pcd = o3d.geometry.PointCloud()
@@ -70,7 +106,7 @@ if __name__ == "__main__":
     # ipdb.set_trace()
 
     o3d.visualization.draw_geometries(
-        [pcd] + kinects,
+        [pcd, robot_mesh] + kinects,
         # [pcd, kinect_mesh, kinect_frame],
         # [pcd, ee_frame, ref_shapes, obbox],
         # zoom=0.2,
